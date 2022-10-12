a29caf286f1c0f8e4c2e79323da185a4574544e4
Khushi Jaiswal <14khushijais@gmail.com>
2022-10-12 20:49:53 +0100

    add ValueError to prime numbers exercise

--- primes.py
+++ primes.py
@@ -1,6 +1,31 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
+import math
 
 def primes(number_of_primes):
-    list = []
-    return list
+
+    if number_of_primes > 0:
+        list = []
+        numerator = 1
+        while len(list) < number_of_primes:
+            numerator = numerator+1
+            for denominator in range (2,numerator):
+                if (numerator%denominator == 0):
+                    break
+            else:
+                list.append(numerator)
+                print(numerator)
+        return list
+
+    if number_of_primes <= 0:
+        raise ValueError ("Parameter must be greater than 0")
+
+
+
+    #print(list)
+
+#primes(-1)
+
+
+    #except ValueError:
+        #raise ValueError("Make sure you dont enter 0 or a negative number")
